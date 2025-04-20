e3910538582268a3383ee0d23fdec67a518b84d8
5tasiu <jez9.stas@gmail.com>
2025-04-20 18:25:50 +0200

    test fix

--- test/test_main.py
+++ test/test_main.py
@@ -25,14 +25,16 @@ class TestPostSummarizer(unittest.TestCase):
 
         summarizer_test = PostSummarizer(genai_mock, bsky_mock)
         posts = summarizer_test.get_latest_posts("UCL")
-
         summarizer_test.generate_response(posts)
 
-#        self.assertEqual(len(posts), 2)
 
         genai_mock.models.generate_content.assert_called_with(
             model='gemini-2.0-flash',
             contents="summarize these posts:  ['[New Post] Test User 1: This is a test post about Elon Musk', '[New Post] Test User 2: Another test post about Elon Musk']")
+
+    def test_clients(self):
+        genai_mock = MagicMock()
+        bsky_mock = MagicMock()
 
         with self.assertRaises(ValueError) as context:
             PostSummarizer(None, bsky_mock)
@@ -42,5 +44,13 @@ class TestPostSummarizer(unittest.TestCase):
             PostSummarizer(genai_mock, None)
         self.assertEqual(str(context.exception), "BSKYPASS environment variable is not set")
 
-        if len(posts) == 0:
-            genai_mock.models.generate_response.assert_not_called()
+    def test_response(self):
+        genai_mock = MagicMock()
+        bsky_mock = MagicMock()
+        mock_search_posts = MagicMock()
+        mock_search_posts.posts = []
+        summarizer_test = PostSummarizer(genai_mock, bsky_mock)
+        posts = summarizer_test.get_latest_posts("UCL")
+        summarizer_test.generate_response(posts)
+
+        genai_mock.models.generate_response.assert_not_called()
